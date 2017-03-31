ed37ccf0b4d4b834667d245751a21b29795e82f2
shelby-toews <shelby.lane.115@gmail.com>
2017-03-31 11:16:16 -0700

    Update CardManager.java

--- CardManager.java
+++ CardManager.java
@@ -2,8 +2,10 @@ package com.example.shelby.yu_gi_ohdueltracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CardManager extends AppCompatActivity {
 
@@ -16,8 +18,21 @@ public class CardManager extends AppCompatActivity {
     }
 
     private void addItem() {
-        View newView;
+        final ViewGroup newView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.activity_card_manager, mContainerView, false);
+
+        ((EditText) newView.findViewById(android.R.id.text1)).setText("");
+
+        newView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContainerView.removeView(newView);
+
+                if (mContainerView.getChildCount() == 0) {
+                    findViewById(android.R.id.empty).setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         mContainerView.addView(newView, 0);
     }
-}
